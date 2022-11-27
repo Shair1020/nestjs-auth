ba024a21b1f5e86dc3b27f6ca4dd1a1b328aa6ed
shair1020 <ali99shair@gmail.com>
2022-11-28 01:51:32 +0500

    create a login and signup api not testing done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_STRING),
+    UsersModule,
+    AuthModule,
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common"
+import { AuthService } from "./auth.service"
+import { PassportModule } from "@nestjs/passport"
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from "@nestjs/mongoose"
+import { UserSchema } from "src/users/users.model";
+import { UsersService } from "src/users/users.service";
+import { LocalStrategy } from "src/local.auth";
+import { UsersModule } from "src/users/users.module";
+
+
+@Module({
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    secret: 'secretKey',
+    signOptions: { expiresIn: '60s' },
+  }), MongooseModule.forFeature([{ name: "user", schema: UserSchema }])],
+  providers: [AuthService, UsersService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule { }
